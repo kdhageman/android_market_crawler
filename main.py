2d981b5cb3ae26313f8bdc2063aab1400775c3cc
kdhageman <kdhageman@users.noreply.github.com>
2019-11-20 16:03:19 +0100

    Add initial Tencent implementation

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
+import os
+
 from scrapy.crawler import CrawlerProcess
 
 from spider.fdroid import FDroidSpider
+from spider.tencent import TencentSpider
 
 
 def main():
     item_pipelines = {
-        'spider.pipeline.DownloadApksPipeline': 100
+        # 'spider.pipeline.DownloadApksPipeline': 100
     }
+
+    feed_uri = f"file://{os.path.join(os.getcwd(), 'meta.csv')}"
 
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-        'CONCURRENT_REQUESTS': 1,
+        'CONCURRENT_REQUESTS': 5,
         'ITEM_PIPELINES': item_pipelines,
         'DEPTH_LIMIT': 2,
-        'FEED_URI': 'file:///meta.csv',
+        'FEED_URI': feed_uri,
         'FEED_EXPORT_FIELDS': ["meta"],
     })
 
-    process.crawl(FDroidSpider)
+    process.crawl(TencentSpider)
     process.start()  # the script will block here until the crawling is finished
 
 
--- spider/tencent.py
+++ spider/tencent.py
@@ -1,4 +1,8 @@
+import re
+
 import scrapy
+
+pkg_pattern = "https://android\.myapp\.com/myapp/detail\.htm\?apkName=(.*)"
 
 
 class TencentSpider(scrapy.Spider):
@@ -6,14 +10,47 @@ class TencentSpider(scrapy.Spider):
     start_urls = ['https://android.myapp.com/']
 
     def parse(self, response):
-        # find all links to
-        for link in response.css("a::attr(href)").re("../myapp/detail.htm\?apkName=.*"):
+        # find links to other apps
+        for link in response. \
+                css("a::attr(href)"). \
+                re("../myapp/detail.htm\?apkName=.*"):
             next_page = response.urljoin(link)  # build absolute URL based on relative link
-            yield scrapy.Request(next_page, callback=self.parse)  # add URL to set of URLs to crawl
+            yield scrapy.Request(next_page, callback=self.parge_pkg_page)  # add URL to set of URLs to crawl
 
+    def parge_pkg_page(self, response):
         # find meta data
-        # TODO: implement
+        meta = dict(
+            pkg_name="",
+            app_name="",
+            app_description="",
+            version="",
+            publish_time="",
+            developer_name=""
+        )
 
-        # find download button
-        for url in response.css("a::attr(data-apkurl"):
-            yield {"datap-apkurl": url}
+        divs = response.css("div.det-othinfo-container").css("div.det-othinfo-data")
+
+        if len(divs) == 4:
+            meta['version'] = divs[0].css("::text").get()
+            meta['publish_time'] = divs[1].attrib['data-apkpublishtime']
+            meta['developer_name'] = divs[2].css("::text").get()
+        else:
+            print("failed to find part of metadata")
+
+        meta['app_name'] = response.css("div.det-name-int::text").get()
+        meta['app_description'] = response.css("div.det-app-data-info::text").get()
+
+        m = re.search(pkg_pattern, response.url)
+        if m:
+            meta['pkg_name'] = m.group(1)
+
+        res = dict(
+            meta=meta,
+            download_urls=[]
+        )
+
+        # find download button(s)
+        for dl_link in response.css("a::attr(data-apkurl)").getall():
+            res["download_urls"].append(dl_link)
+
+        return res
