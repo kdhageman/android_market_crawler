8e22cff882329f175dd8c115c792e270106f1ea9
kdhageman <kdhageman@users.noreply.github.com>
2019-11-26 16:38:11 +0100

    Randomize user agents

--- main.py
+++ main.py
@@ -32,8 +32,40 @@ def main():
     feed_uri = f"file://{os.path.join(os.getcwd(), 'meta.csv')}"
     outdir = conf['outdir']
 
+    downloader_middlewares = {
+        'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+        'scrapy_useragents.downloadermiddlewares.useragents.UserAgentsMiddleware': 500,
+    }
+
+    user_agents = [
+        ('Mozilla/5.0 (X11; Linux x86_64) '
+         'AppleWebKit/537.36 (KHTML, like Gecko) '
+         'Chrome/57.0.2987.110 '
+         'Safari/537.36'),  # chrome
+        ('Mozilla/5.0 (X11; Linux x86_64) '
+         'AppleWebKit/537.36 (KHTML, like Gecko) '
+         'Chrome/61.0.3163.79 '
+         'Safari/537.36'),  # chrome
+        ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) '
+         'Gecko/20100101 '
+         'Firefox/55.0'),  # firefox
+        ('Mozilla/5.0 (X11; Linux x86_64) '
+         'AppleWebKit/537.36 (KHTML, like Gecko) '
+         'Chrome/61.0.3163.91 '
+         'Safari/537.36'),  # chrome
+        ('Mozilla/5.0 (X11; Linux x86_64) '
+         'AppleWebKit/537.36 (KHTML, like Gecko) '
+         'Chrome/62.0.3202.89 '
+         'Safari/537.36'),  # chrome
+        ('Mozilla/5.0 (X11; Linux x86_64) '
+         'AppleWebKit/537.36 (KHTML, like Gecko) '
+         'Chrome/63.0.3239.108 '
+         'Safari/537.36'),  # chrome
+    ]
+
     process = CrawlerProcess(dict(
-        USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+        DOWNLOADER_MIDDLEWARES=downloader_middlewares,
+        USER_AGENTS=user_agents,
         CONCURRENT_REQUESTS=1,
         ITEM_PIPELINES=item_pipelines,
         DEPTH_LIMIT=1,
