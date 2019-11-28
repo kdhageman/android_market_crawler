52548a958ef111b560c1279773d9877d785c8cd8
kdhageman <kdhageman@users.noreply.github.com>
2019-11-28 10:59:49 +0100

    Update ApkMirror crawler

--- pystorecrawler/spider/apkmirror.py
+++ pystorecrawler/spider/apkmirror.py
@@ -43,7 +43,8 @@ class ApkMirrorSpider(scrapy.Spider):
         variant_link = list_widgets[0].css("div.table a::attr(href)").get()  # get the first variants link, we don't care about the various variants
         if variant_link:
             full_link = response.urljoin(variant_link)
-            yield scrapy.Request(full_link, callback=self.parse_download_page)
+            # give higher priority to package download pages
+            yield scrapy.Request(full_link, callback=self.parse_download_page, priority=1)
 
         # find all version links, list with 'All Versions ' or 'All Releases ' header
         list_of_other_versions = response.xpath("//div[@class='listWidget' and .//div[@class='widgetHeader' and (contains(text(), 'All Releases ') or contains(text(), 'All versions '))]]")
@@ -80,6 +81,17 @@ class ApkMirrorSpider(scrapy.Spider):
 
         m = appspecs[-1].css("::text").re(" by (.*)")
         meta['uploader'] = m[0] if m else None
+
+        meta['downloads'] = appspecs[0].css("::text")[-1].re("(.*) downloads")[0]
+
+        user_rating = response.xpath("//div[@itemprop = 'aggregateRating']/span[1]/span[1]//@title").re("(.*) / 5.0")[0]
+        meta['user_rating'] = normalize_rating(user_rating)
+
+        category = response.css("a.play-category::text").get()
+        meta['categories'] = [category]
+
+        icon_url_rel = response.css("div.siteTitleBar img::attr(src)").get()
+        meta['icon_url'] = response.urljoin(icon_url_rel)
 
         # find download link
         versions = dict()
@@ -119,3 +131,15 @@ class ApkMirrorSpider(scrapy.Spider):
         if next_page_link:
             full_link = response.urljoin(next_page_link)
             yield scrapy.Request(full_link, callback=self.parse_versions_page)
+
+def normalize_rating(rating):
+    """
+    Normalizes a (string) rating between 0 and 5 to a float between 0 and 100
+    Args:
+        rating : str
+            between 0 and 5
+
+    Returns: float
+        between 0 and 100
+    """
+    return float(rating) * 20
