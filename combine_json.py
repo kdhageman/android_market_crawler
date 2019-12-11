7626403e77b8ee782f573658744833e520cf6917
kdhageman <kdhageman@users.noreply.github.com>
2019-12-11 12:08:50 +0100

    Fix JSON reading

--- combine_json.py
+++ combine_json.py
@@ -2,6 +2,45 @@ import argparse
 import os
 from tqdm import tqdm
 
+
+def last_json_from_file(f):
+    """
+    Reads last JSON from file f
+    Allows reading files that do not separate JSON content by a newline character
+    """
+    c = f.read(1)
+    bcount = 0  # bracket count
+    last_json = ""
+    buf = ""
+    while c:
+        buf += c
+        if c == "{":
+            bcount += 1
+        elif c == "}":
+            bcount -= 1
+            if bcount == 0:
+                # last bracket
+                last_json = buf
+                buf = ""
+        c = f.read(1)
+    return last_json
+
+
+def walk_spider_dir(spiderdir, spider):
+    """
+    Generator
+    """
+    pkgs = [d for d in os.listdir(spiderdir) if os.path.isdir(os.path.join(spiderdir, d))]
+    for pkg in tqdm(pkgs, desc=spider, total=len(pkgs)):
+        walkdir = os.path.join(spiderdir, pkg)
+        for root, dirs, files in os.walk(walkdir):
+            for file in files:
+                if file == "meta.json":
+                    fullpath = os.path.join(root, file)
+                    with open(fullpath, "r") as f:
+                        yield f
+
+
 def main(args):
     with open(args.spidertxt, "r") as f:
         spiders = [l.strip() for l in f.readlines()]
@@ -11,21 +50,14 @@ def main(args):
         spiderdir = os.path.join(args.dir, spider)
 
         if os.path.exists(spiderdir):
-            pkgs = [d for d in os.listdir(spiderdir) if os.path.isdir(os.path.join(spiderdir, d))]
-            for pkg in tqdm(pkgs, desc=spider, total=len(pkgs)):
-                walkdir = os.path.join(spiderdir, pkg)
-                for root, dirs, files in os.walk(walkdir):
-                    for file in files:
-                        if file == "meta.json":
-                            fullpath = os.path.join(root, file)
-                            with open(fullpath, "r") as f:
-                                content = f.readlines()
-                                if content:
-                                    json_lines.append(content[-1])
+            for f in walk_spider_dir(spiderdir, spider):
+                json_line = last_json_from_file(f)
+                json_lines.append(json_line)
 
     with open(args.outfile, "w") as f:
         for json_line in json_lines:
             f.write(json_line + "\n")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Aggregates JSON outputs from "run_spider.py" scripts')
