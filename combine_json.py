aeb3aad8fc18ebacdb1a8911ce6ba9f2d26dd727
kdhageman <kdhageman@users.noreply.github.com>
2019-12-11 10:02:18 +0100

    Add script to combine all generated meta.json files
    Only reads the last line from each meta.json file

---
+++ combine_json.py
@@ -0,0 +1,33 @@
+import argparse
+import os
+
+def main(args):
+    with open(args.spidertxt, "r") as f:
+        spiders = [l.strip() for l in f.readlines()]
+
+    json_lines = []
+    for spider in spiders:
+        walkdir = os.path.join(args.dir, spider)
+
+        if os.path.exists(walkdir):
+            for root, dirs, files in os.walk(walkdir):
+                for file in files:
+                    if file == "meta.json":
+                        fullpath = os.path.join(root, file)
+                        with open(fullpath, "r") as f:
+                            content = f.readlines()
+                            if content:
+                                json_lines.append(content[-1])
+
+    with open(args.outfile, "w") as f:
+        for json_line in json_lines:
+            f.write(json_line + "\n")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Aggregates JSON outputs from "run_spider.py" scripts')
+    parser.add_argument("--dir", help="Directory to traverse", default=".")
+    parser.add_argument("--outfile", help="Name of output file", default="out.json")
+    parser.add_argument("--spidertxt", help="File which contains names of spiders", default="spider_list.txt")
+    args = parser.parse_args()
+
+    main(args)
