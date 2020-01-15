a22155cd8a4a3a93e38df5692301e437618075a7
kdhageman <kdhageman@users.noreply.github.com>
2020-01-15 13:41:09 +0100

    Initial implementation using pg8000

--- crawler/pipelines/database.py
+++ crawler/pipelines/database.py
@@ -1,40 +1,112 @@
 import os
 import sqlite3
 
+import pg8000
+
 from crawler.item import Result
 from crawler.util import market_from_spider
 
-_tables = [
+_sqlite_tables = [
     ("apks", "sha256 text, path text"),
     ("packages", "pkg_name text, id text, market text, timestamp int"),
     ("versions", "pkg_name text, id text, version text, market text, sha256 text"),
 ]
 
+_postgres_tables = [
+    ("apks", "sha256 char(256), path text"),
+    ("packages", "pkg_name text, id text, market varchar(32), timestamp int"),
+    ("versions", "pkg_name text, id text, version text, market varchar(32), sha256 char(256)"),
+]
 
-def path_by_sha(conn, sha):
-    qry = "SELECT path FROM apks WHERE sha256 = ?"
-    with conn:
-        res = conn.execute(qry, (sha,))
-        first = res.fetchone()
-        return first[0] if first else None
+_tables_from_dbtype = {
+    "sqlite": _sqlite_tables,
+    "postgres": _postgres_tables
+}
 
 
-class SqlitePipeline:
-    def __init__(self, dbfile):
-        self.conn = sqlite3.connect(dbfile)
-        for table, fields in _tables:
+class InvalidParametersError(Exception):
+    pass
+
+
+def _conn_from_params(params):
+    """
+    Return the database connection given the database settings of the crawler
+    Args:
+        params: dict
+
+    Returns: database connection
+    """
+    dbtype = params.get("type", None)
+    db_specific_params = params.get(dbtype, None)
+    if dbtype == "sqlite":
+        filename = db_specific_params.get("dbfile", None)
+        conn = sqlite3.connect(filename)
+    elif dbtype == "postgres":
+        conn = pg8000.connect(**db_specific_params)
+    else:
+        raise InvalidParametersError
+
+    if not db_specific_params:
+        pass
+    return conn, dbtype
+
+
+class _DatabaseConnection:
+    def __init__(self, conn):
+        self.conn = conn
+
+    def __enter__(self):
+        self.cur = self.conn.cursor()
+        return self.cur
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_tb is None:
+            self.conn.commit()
+        else:
+            self.conn.rollback()
+
+
+class DatabasePipeline:
+    def __init__(self, conn, dbtype):
+        self.conn = conn
+        self.dbtype = dbtype
+
+        tables = _tables_from_dbtype[dbtype]
+        for table, fields in tables:
             qry = f"CREATE TABLE IF NOT EXISTS {table} ({fields})"
-            with self.conn:
-                self.conn.execute(qry)
+            with _DatabaseConnection(self.conn) as cur:
+                cur.execute(qry)
+
+    def path_by_sha(self, sha):
+        qry = "SELECT path FROM apks WHERE sha256 = ?"
+        with _DatabaseConnection(self.conn) as cur:
+            res = cur.execute(qry, (sha,))
+            first = res.fetchone()
+            return first[0] if first else None
+        return None
+
+    def insert(self, table, values=()):
+        if self.dbtype == "sqlite":
+            qry = f"INSERT INTO {table} VALUES ({', '.join(['?' for v in values])})"
+        elif self.dbtype == "postgres":
+            qry = f"INSERT INTO {table} VALUES {', '.join(['(%s)' for v in values])}"
+        with _DatabaseConnection(self.conn) as cur:
+            return cur.execute(qry, values)
+
+    def close_spider(self, spider):
+        self.cur.close()
+        self.conn.close()
 
 
-class PreDownloadPackagePipeline(SqlitePipeline):
-    def __init__(self, dbfile="crawl.db"):
-        super().__init__(dbfile)
+class PreDownloadPackagePipeline(DatabasePipeline):
+    def __init__(self, conn, dbtype):
+        super().__init__(conn, dbtype)
 
     @classmethod
     def from_crawler(cls, crawler):
-        return cls(**crawler.settings.get("SQLITE_PARAMS"))
+        params = crawler.settings.get("DATABASE_PARAMS")
+        conn, dbtype = _conn_from_params(params)
+        return cls(conn, dbtype)
 
     def process_item(self, item, spider):
         if not isinstance(item, Result):
@@ -50,22 +122,22 @@ class PreDownloadPackagePipeline(SqlitePipeline):
         identifier = meta.get("id", None)
         pkg_name = meta.get("pkg_name", None)
         ts = meta.get('timestamp', 0)
-        qry = "INSERT INTO packages VALUES (?, ?, ?, ?)"
-        with self.conn:
-            self.conn.execute(qry, (pkg_name, identifier, market, ts))
+        self.insert("packages", (pkg_name, identifier, market, ts))
 
 
-class PreDownloadVersionPipeline(SqlitePipeline):
+class PreDownloadVersionPipeline(DatabasePipeline):
     """
     Checks if the APK for a specific version has already been downloaded or not
     """
 
-    def __init__(self, dbfile="crawl.db"):
-        super().__init__(dbfile)
+    def __init__(self, conn, dbtype):
+        super().__init__(conn, dbtype)
 
     @classmethod
     def from_crawler(cls, crawler):
-        return cls(**crawler.settings.get("SQLITE_PARAMS"))
+        params = crawler.settings.get("DATABASE_PARAMS")
+        conn, dbtype = _conn_from_params(params)
+        return cls(conn, dbtype)
 
     def process_item(self, item, spider):
         if not isinstance(item, Result):
@@ -81,7 +153,7 @@ class PreDownloadVersionPipeline(SqlitePipeline):
             existing_sha = self.version_exists(pkg_name, identifier, version, market)
             if existing_sha:
                 spider.logger.info(f"seen version '{version}' of '{pkg_name if pkg_name else identifier}' before")
-                path = path_by_sha(self.conn, existing_sha)
+                path = self.path_by_sha(existing_sha)
                 dat['skip'] = True
                 dat['file_sha256'] = existing_sha
                 dat['file_path'] = path
@@ -93,24 +165,28 @@ class PreDownloadVersionPipeline(SqlitePipeline):
         """
         Returns the SHA256 value of the apk for the given tuple of values
         """
+        self.select(["sha256"], "versions", ["pkg_name", "id", "version", "market"])
         qry = "SELECT sha256 FROM versions WHERE (pkg_name = ? OR id = ?) AND version = ? and market = ?"
-        with self.conn:
-            res = self.conn.execute(qry, (pkg_name, identifier, version, market))
+        with _DatabaseConnection(self.conn) as cur:
+            res = cur.execute(qry, (pkg_name, identifier, version, market))
             first = res.fetchone()
             return first[0] if first else None
+        return None
 
 
-class PostDownloadPipeline(SqlitePipeline):
+class PostDownloadPipeline(DatabasePipeline):
     """
     Ensures that (1) duplicate APKs cleaned up and (2) crawls are logged in the database
     """
 
-    def __init__(self, dbfile="crawl.db"):
-        super().__init__(dbfile)
+    def __init__(self, conn, dbtype):
+        super().__init__(conn, dbtype)
 
     @classmethod
     def from_crawler(cls, crawler):
-        return cls(**crawler.settings.get("SQLITE_PARAMS"))
+        params = crawler.settings.get("DATABASE_PARAMS")
+        conn, dbtype = _conn_from_params(params)
+        return cls(conn, dbtype)
 
     def process_item(self, item, spider):
         if not isinstance(item, Result):
@@ -135,7 +211,7 @@ class PostDownloadPipeline(SqlitePipeline):
                 self.create_version(pkg_name, identifier, version, market, sha)
 
             # check if another APK with same hash exists
-            existing_path = path_by_sha(self.conn, sha)
+            existing_path = self.path_by_sha(sha)
             if not existing_path:
                 spider.logger.info(f"creating unseen path for '{sha}'")
                 self.create_sha(sha, path)
