7706196aaee4af3ebb6d1f225d5f8084858b9c6c
kdhageman <kdhageman@users.noreply.github.com>
2020-10-11 12:42:15 +0200

    Parallelize with multprocessing

--- scripts/add_signers_to_analysis.py
+++ scripts/add_signers_to_analysis.py
@@ -3,7 +3,9 @@ import json
 import logging
 import math
 import os
+import queue
 import sys
+from multiprocessing import Queue, Process
 
 from sqlalchemy import create_engine, text
 
@@ -12,6 +14,56 @@ from crawler.pipelines.analyze_apks import analyse
 
 _version_table = "versions"
 _column = "meta_new"
+
+
+def update_analysis(q_tasks, q_progress, db_string):
+    engine = create_engine(db_string)
+
+    while True:
+        try:
+            idx, m = q_tasks.get_nowait()
+        except queue.Empty:
+            break
+
+        meta, versions = m.values()
+
+        for v, dat in versions.items():
+            file_path = dat.get("file_path", "")
+            new_analysis = analyse(file_path)
+            dat['analysis'] = new_analysis
+            versions[v] = dat
+
+        jsonstr = json.dumps(dict(meta=meta, versions=versions))
+
+        with engine.connect() as conn:
+            qry = text(f"UPDATE {_version_table} SET {_column} = :jsonstr WHERE id = :idx")
+            vals = dict(
+                idx=idx,
+                jsonstr=jsonstr
+            )
+            conn.execute(qry, vals)
+
+        # bump progress
+        q_progress.put(None)
+
+
+def progress(q, rowcount):
+    i = 0
+    perc_limit = math.ceil(rowcount / 1000)
+
+    while True:
+        try:
+            q.get()
+        except queue.Empty:
+            break
+
+        i += 1
+
+        if i % perc_limit == 0:
+            perc = i / rowcount * 100
+            msg = f"{perc:.2f}%"
+            print(msg)
+
 
 
 def main(args):
@@ -36,35 +88,35 @@ def main(args):
 
     log.info("Fetching from database..")
     with engine.connect() as conn:
-        qry = f"SELECT id, meta FROM versions WHERE meta is not null AND {_column} is null"
+        # qry = f"SELECT id, meta FROM versions WHERE meta is not null AND {_column} is null"
+        qry = f"SELECT id, meta FROM versions WHERE meta is not null LIMIT 10"
         result_set = conn.execute(qry)
     log.info("Fetched from database!")
 
-    rowcount = result_set.rowcount
-    perc_limit = math.ceil(rowcount / 1000)
+    q_tasks = Queue()
+    q_progress = Queue()
 
-    for i, (idx, m) in enumerate(result_set):
-        meta, versions = m.values()
+    processes = []
 
-        for v, dat in versions.items():
-            file_path = dat.get("file_path", "")
-            new_analysis = analyse(file_path)
-            dat['analysis'] = new_analysis
-            versions[v] = dat
+    # fill queue of tasks
+    for idx, m in result_set:
+        task = (idx, m)
+        q_tasks.put(task)
 
-        jsonstr = json.dumps(dict(meta=meta, versions=versions))
+    # create task processes
+    for i in range(args.nprocesses):
+        p = Process(target=update_analysis, args=(q_tasks, q_progress, db_string))
+        processes.append(p)
+        p.start()
 
-        with engine.connect() as conn:
-            qry = text(f"UPDATE {_version_table} SET {_column} = :jsonstr WHERE id = :idx")
-            vals = dict(
-                idx=idx,
-                jsonstr=jsonstr
-            )
-            conn.execute(qry, vals)
+    # create progress process
+    p_progress = Process(target=progress, args=(q_progress, result_set.rowcount))
+    p_progress.start()
 
-        if i % perc_limit == 0:
-            perc = i / rowcount * 100
-            log.info(f"{perc:.2f}%")
+    for p in processes:
+        p.join()
+
+    p_progress.terminate()
 
 
 if __name__ == "__main__":
@@ -75,6 +127,7 @@ if __name__ == "__main__":
     parser.add_argument("--postgres-host", default="127.0.0.1")
     parser.add_argument("--postgres-port", default=5432)
     parser.add_argument("--postgres-db", default="postgres")
+    parser.add_argument("--nprocesses", default=1, type=int)
 
     args = parser.parse_args()
 
